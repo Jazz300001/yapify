7c8d7bc83c73ba5a65ad49c3b9ede287f1d650aa
Jazz300001 <98571245+Jazz300001@users.noreply.github.com>
2025-06-27 14:41:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ import json
 import praw
 import feedparser
 import asyncio
+from keep_alive import keep_alive
+keep_alive()
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+token  = os.getenv('DISCORD_TOKEN')
 RED_ID = os.getenv('RED_ID')
 RED_SECRET = os.getenv('RED_SECRET')
 discwebhook = os.getenv("DISCWEBHOOK")
@@ -31,7 +33,6 @@ print(allrss)
 discchannel = 376203676215148546
 
 from discord.ext import commands
-
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
@@ -39,11 +40,9 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-
 @bot.command()
 async def ping(ctx):
     await ctx.send("Pong!")
-
 
 @bot.event
 async def on_ready():
@@ -54,7 +53,6 @@ async def on_ready():
     except Exception as e:
         print(f"Sync failed: {e}")
     ytcheck.start()
-
 
 @tasks.loop(minutes=10)
 async def ytcheck():
@@ -67,21 +65,23 @@ async def ytcheck():
             lastvids[rss] = recent.id
             channel = bot.get_channel(discchannel)
             await channel.send(f"peep da vid-- {recent.title}\n{recent.link}")
-
+        else:
+            channel = bot.get_channel(discchannel)
+            channel.send("nothing new yet")
 
 @bot.event
 async def on_member_join(member):
     await member.send(f"whip it out and spin it, {member.name}")
 
-
 @bot.event
 async def on_message(message):
+    othpic = "https://tenor.com/view/funny-gif-2583399619372591816"
+    print(othpic)
     if message.author == bot.user:
         return
     if "watch" in message.content.lower():
-        await message.channel.send(f"{message.author.mention} aw hell nah he watchin??")
+        await message.channel.send(f"{message.author.mention} aw hell nah he watchin??\n{othpic}")
     await bot.process_commands(message)
-
 
 @bot.tree.command()
 async def spitskyrim(interaction: discord.Interaction):
@@ -89,9 +89,7 @@ async def spitskyrim(interaction: discord.Interaction):
     with open(file_path, 'r', encoding='utf-8') as f:
         lore_data = json.load(f)["quotes"]
     quote = random.choice(lore_data)
-    await interaction.response.send_message(
-        f"I used to be an adventurer like you until I took an arrow to the knee,\n{quote}")
-
+    await interaction.response.send_message(f"I used to be an adventurer like you until I took an arrow to the knee,\n{quote}")
 
 @bot.tree.command()
 async def spitsw(interaction: discord.Interaction):
@@ -100,7 +98,6 @@ async def spitsw(interaction: discord.Interaction):
         lore_data = json.load(f)["quotes"]
     quote = random.choice(lore_data)
     await interaction.response.send_message(f"May the force be with you,\n{quote}")
-
 
 @bot.tree.command()
 async def spitoog(interaction: discord.Interaction):
@@ -131,17 +128,19 @@ async def bother(interaction: discord.Interaction, user: discord.User):
         pick = random.choice(videos)
         video = os.path.join(f"vids", pick)
         file = discord.File(video, filename=pick)
-        await user.send(f"This was not sent to bother you,", file=file)
+        await user.send(f"This was not sent to bother you,",file = file)
         await interaction.response.send_message("No I cant laugh yet, Ive got to hold it in", ephemeral=True)
     except discord.Forbidden:
         await interaction.response.send_message("dm forbidden bro think he mysterious", ephemeral=True)
+
+
+
 
 
 @bot.tree.command()
 async def racist(interaction: discord.Interaction):
     gif_url = "https://cdn.discordapp.com/attachments/1202760576300879964/1379988483707502602/IMG_6057.png?ex=684e1b6d&is=684cc9ed&hm=996d7a5c20dd75642087b455863f35a8134c00b6b88001e57c40a4964296fdd8&"
     await interaction.response.send_message(gif_url)
-
 
 @bot.tree.command()
 async def pinned(interaction: discord.Interaction):
@@ -164,22 +163,20 @@ async def pinned(interaction: discord.Interaction):
 
 reddit = praw.Reddit(
     client_id=RED_ID,
-    client_secret=RED_SECRET,
-    user_agent="discord bot by /u/Ok-Walk1768"
+    client_secret = RED_SECRET,
+    user_agent = "discord bot by /u/Ok-Walk1768"
 )
-
-
 @bot.tree.command()
-async def red(interaction: discord.Interaction, sub: str, sort: str = "hot", numofposts: str = "3"):
+async def red(interaction: discord.Interaction, sub:str, sort:str = "hot", numofposts:str = "3"):
     await interaction.response.defer()
     try:
         sub = reddit.subreddit(sub)
         if sort == "hot":
-            posts = sub.hot(limit=int(numofposts))
+            posts = sub.hot(limit = int(numofposts))
         elif sort == "top":
-            posts = sub.top(limit=int(numofposts))
+            posts = sub.top(limit = int(numofposts))
         elif sort == "new":
-            posts = sub.new(limit=int(numofposts))
+            posts = sub.new(limit = int(numofposts))
         else:
             await interaction.followup.send(f"Sorry, {sort} isn't a valid sort.")
             return
@@ -209,6 +206,7 @@ async def red(interaction: discord.Interaction, sub: str, sort: str = "hot", num
             await interaction.followup.send(embed=embed)
     except Exception as e:
         print(e)
+        await interaction.followup.send("An error occurred while fetching the posts.")
 
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
